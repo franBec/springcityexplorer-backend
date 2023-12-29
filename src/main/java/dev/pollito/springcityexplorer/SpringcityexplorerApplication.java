7743ea4e9df5ca91753a1f31026ff12da03fdf42
Fran Bec <franbecvort@gmail.com>
2023-12-29 16:02:48 +0000

    Initial commit

---
+++ src/main/java/dev/pollito/springcityexplorer/SpringcityexplorerApplication.java
@@ -0,0 +1,13 @@
+package dev.pollito.springcityexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcityexplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcityexplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pollito/springcityexplorer/SpringcityexplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pollito.springcityexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcityexplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
