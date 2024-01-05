57e645b722100d02da1ac8cb2a6787e2e468d87d
Fran Bec <franbecvort@gmail.com>
2024-01-05 20:40:34 +0000

    now when posting a comment, it returns the comment just saved

--- src/main/java/dev/pollito/springcityexplorer/controller/CommentController.java
+++ src/main/java/dev/pollito/springcityexplorer/controller/CommentController.java
@@ -1,7 +1,8 @@
 package dev.pollito.springcityexplorer.controller;
 
 import dev.pollito.springcityexplorer.api.CommentApi;
-import dev.pollito.springcityexplorer.models.CommentPostBody;
+import dev.pollito.springcityexplorer.models.CommentPostRequest;
+import dev.pollito.springcityexplorer.models.CommentPostResponse;
 import dev.pollito.springcityexplorer.models.Comments;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ public class CommentController implements CommentApi {
   }
 
   @Override
-  public ResponseEntity<Void> postComment(CommentPostBody body) {
+  public ResponseEntity<CommentPostResponse> postComment(CommentPostRequest body) {
     return null;
   }
 }
