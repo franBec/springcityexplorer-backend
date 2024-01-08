8c84b417e36683336f6c10ad0ec494a4103b0ead
Fran Bec <franbecvort@gmail.com>
2024-01-08 17:47:27 +0000

    Improved comments validations

--- src/main/java/dev/pollito/springcityexplorer/service/CommentService.java
+++ src/main/java/dev/pollito/springcityexplorer/service/CommentService.java
@@ -6,12 +6,13 @@ import dev.pollito.springcityexplorer.models.CommentPostResponse;
 import dev.pollito.springcityexplorer.models.Comments;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 public interface CommentService {
   Comments getComments(
-      @Max(100) Integer limit, Integer offset, @ValidCommentSortOrder String sortOrder);
+          @Max(100) @Min(1) Integer limit, @Min(0) Integer offset, @ValidCommentSortOrder String sortOrder);
 
   CommentPostResponse postComment(@Valid CommentPostRequestJakarta request);
 }
