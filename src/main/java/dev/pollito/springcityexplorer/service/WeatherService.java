b019f2cedaabdfbca66dac9df7e1abd55d8531a3
Fran Bec <franbecvort@gmail.com>
2024-01-08 17:55:59 +0000

    Weather Validations

--- src/main/java/dev/pollito/springcityexplorer/service/WeatherService.java
+++ src/main/java/dev/pollito/springcityexplorer/service/WeatherService.java
@@ -1,7 +1,10 @@
 package dev.pollito.springcityexplorer.service;
 
 import dev.pollito.springcityexplorer.models.Weather;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 public interface WeatherService {
-  Weather getWeatherByCity(String city);
+  Weather getWeatherByCity(@NotBlank String city);
 }
