d885ce30a358648aba7eca737b453ea444f24aba
Fran Bec <franbecvort@gmail.com>
2024-01-17 12:47:40 +0000

    first success scenario, though snake_case values came null
    - null values that shouldn't: timezone_id, localtime, localtime_epoch, utc_offset, observation_time, weather_code, weather_icons, weather_descriptions, wind_degree, wind_dir, uv_index

--- src/main/java/dev/pollito/springcityexplorer/service/impl/WeatherServiceImpl.java
+++ src/main/java/dev/pollito/springcityexplorer/service/impl/WeatherServiceImpl.java
@@ -1,13 +1,28 @@
 package dev.pollito.springcityexplorer.service.impl;
 
+import com.weatherstack.api.WeatherApi;
+import com.weatherstack.models.WeatherStackCurrentWeather;
+import dev.pollito.springcityexplorer.client.WeatherClient;
+import dev.pollito.springcityexplorer.config.properties.WeatherProperties;
 import dev.pollito.springcityexplorer.models.Weather;
 import dev.pollito.springcityexplorer.service.WeatherService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class WeatherServiceImpl implements WeatherService {
+
+  private final WeatherClient weatherClient;
+  private final WeatherProperties weatherProperties;
+
   @Override
   public Weather getWeatherByCity(String city) {
+    WeatherStackCurrentWeather weather =
+        weatherClient.currentGet(
+            new WeatherApi.CurrentGetQueryParams()
+                .accessKey(weatherProperties.getSecrets().get("key"))
+                .query(city));
     return null;
   }
 }
