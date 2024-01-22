1be60cf87da8c70cae95a47074a7839c01011113
Fran Bec <franbecvort@gmail.com>
2024-01-22 19:32:35 +0000

    extracted string into constants

--- src/main/java/dev/pollito/springcityexplorer/mapper/ErrorMapper.java
+++ src/main/java/dev/pollito/springcityexplorer/mapper/ErrorMapper.java
@@ -8,13 +8,16 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface ErrorMapper {
 
-  @Mapping(expression = "java(java.time.OffsetDateTime.now())", target = "timestamp")
-  @Mapping(expression = "java(e.getClass().getSimpleName())", target = "error")
+  String JAVA_TIME_OFFSET_DATE_TIME_NOW = "java(java.time.OffsetDateTime.now())";
+  String JAVA_E_GET_CLASS_GET_SIMPLE_NAME = "java(e.getClass().getSimpleName())";
+
+  @Mapping(expression = JAVA_TIME_OFFSET_DATE_TIME_NOW, target = "timestamp")
+  @Mapping(expression = JAVA_E_GET_CLASS_GET_SIMPLE_NAME, target = "error")
   @Mapping(source = "e.message", target = "message")
   Error map(Exception e);
 
-  @Mapping(expression = "java(e.getClass().getSimpleName())", target = "error")
-  @Mapping(expression = "java(java.time.OffsetDateTime.now())", target = "timestamp")
+  @Mapping(expression = JAVA_E_GET_CLASS_GET_SIMPLE_NAME, target = "error")
+  @Mapping(expression = JAVA_TIME_OFFSET_DATE_TIME_NOW, target = "timestamp")
   @Mapping(source = "e.weatherStackError.error.type", target = "message")
   Error map(WeatherException e);
 }
