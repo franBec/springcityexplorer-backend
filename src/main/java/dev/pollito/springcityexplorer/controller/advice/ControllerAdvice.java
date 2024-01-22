d9764fbe136682a6c8d9a6e6abf110530f088f01
Fran Bec <franbecvort@gmail.com>
2024-01-22 20:13:55 +0000

    basic handling of bad request city not found

--- src/main/java/dev/pollito/springcityexplorer/controller/advice/ControllerAdvice.java
+++ src/main/java/dev/pollito/springcityexplorer/controller/advice/ControllerAdvice.java
@@ -3,13 +3,13 @@ package dev.pollito.springcityexplorer.controller.advice;
 import dev.pollito.springcityexplorer.exception.WeatherException;
 import dev.pollito.springcityexplorer.mapper.ErrorMapper;
 import feign.codec.DecodeException;
+import java.time.OffsetDateTime;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.Objects;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
@@ -18,16 +18,24 @@ public class ControllerAdvice {
 
   @ExceptionHandler(WeatherException.class)
   public ResponseEntity<Object> handle(WeatherException e) {
-    if(isBadRequest(e)){
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMapper.map(e));
+    if (isWeatherBadRequest(e)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(createBodyWeatherBadRequest(e));
     }
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMapper.map(e));
   }
 
-  private boolean isBadRequest(WeatherException e) {
+  private dev.pollito.springcityexplorer.models.Error createBodyWeatherBadRequest(
+      WeatherException e) {
+    return new dev.pollito.springcityexplorer.models.Error()
+        .timestamp(OffsetDateTime.now())
+        .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+        .message("City not found");
+  }
+
+  private boolean isWeatherBadRequest(WeatherException e) {
     return Objects.nonNull(e.getWeatherStackError().getError())
-            && Objects.nonNull(e.getWeatherStackError().getError().getCode())
-            && 615 == e.getWeatherStackError().getError().getCode();
+        && Objects.nonNull(e.getWeatherStackError().getError().getCode())
+        && 615 == e.getWeatherStackError().getError().getCode();
   }
 
   @ExceptionHandler(Exception.class)
