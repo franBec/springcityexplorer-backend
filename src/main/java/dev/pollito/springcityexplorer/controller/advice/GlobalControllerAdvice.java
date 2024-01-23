7d1a1017e80484752d059380f7ac4c3670267d65
Fran Bec <franbecvort@gmail.com>
2024-01-23 20:15:16 +0000

    error handling finished

--- src/main/java/dev/pollito/springcityexplorer/controller/advice/GlobalControllerAdvice.java
+++ src/main/java/dev/pollito/springcityexplorer/controller/advice/GlobalControllerAdvice.java
@@ -6,7 +6,6 @@ import static dev.pollito.springcityexplorer.util.ControllerAdviceUtil.getGeneri
 import dev.pollito.springcityexplorer.exception.WeatherException;
 import dev.pollito.springcityexplorer.models.Error;
 import feign.codec.DecodeException;
-import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +15,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import java.util.Objects;
-
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalControllerAdvice {
@@ -25,37 +22,23 @@ public class GlobalControllerAdvice {
   private final WeatherControllerAdvice weatherControllerAdvice;
 
   @ExceptionHandler(MissingServletRequestParameterException.class)
-  public ResponseEntity<Error> handle(MissingServletRequestParameterException e){
-    return getBadRequestError(e.getClass().getSimpleName(), e.getBody().getDetail());
+  public ResponseEntity<Error> handle(MissingServletRequestParameterException e) {
+    return getBadRequestError(e);
   }
 
   @ExceptionHandler(ConstraintViolationException.class)
-  public ResponseEntity<Error> handle(ConstraintViolationException e){
-    return getBadRequestError(e.getClass().getSimpleName(), constraintViolationExceptionMessageFormatter(e));
-  }
-
-  private static String constraintViolationExceptionMessageFormatter(ConstraintViolationException e){
-    String formattedMessage = "";
-
-    if (!e.getConstraintViolations().isEmpty()) {
-      ConstraintViolation<?> violation = e.getConstraintViolations().iterator().next();
-      String propertyPath = violation.getPropertyPath().toString();
-      String fieldName = propertyPath.substring(propertyPath.lastIndexOf('.') + 1);
-      String message = violation.getMessage();
-      formattedMessage = fieldName + ": " + message;
-    }
-
-    return formattedMessage;
+  public ResponseEntity<Error> handle(ConstraintViolationException e) {
+    return getBadRequestError(e);
   }
 
   @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-  public ResponseEntity<Error> handle(MethodArgumentTypeMismatchException e){
-    return getBadRequestError(e.getClass().getSimpleName(), e.getCause().getCause().getMessage());
+  public ResponseEntity<Error> handle(MethodArgumentTypeMismatchException e) {
+    return getBadRequestError(e);
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<Error> handle(MethodArgumentNotValidException e){
-    return getBadRequestError(e.getClass().getSimpleName(), Objects.requireNonNull(e.getDetailMessageArguments())[1].toString().replace("[", "").replace("]", ""));
+  public ResponseEntity<Error> handle(MethodArgumentNotValidException e) {
+    return getBadRequestError(e);
   }
 
   @ExceptionHandler(Exception.class)
--- src/main/java/dev/pollito/springcityexplorer/util/ControllerAdviceUtil.java
+++ src/main/java/dev/pollito/springcityexplorer/util/ControllerAdviceUtil.java
@@ -2,16 +2,24 @@ package dev.pollito.springcityexplorer.util;
 
 import dev.pollito.springcityexplorer.exception.WeatherException;
 import dev.pollito.springcityexplorer.models.Error;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import java.time.OffsetDateTime;
+import java.util.Objects;
 import java.util.UUID;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 public class ControllerAdviceUtil {
 
   private ControllerAdviceUtil() {}
 
-  public static final String GENERIC_ERROR_MESSAGE = "An unexpected error occurred. Please try again later.";
+  public static final String GENERIC_ERROR_MESSAGE =
+      "An unexpected error occurred. Please try again later.";
 
   public static final String WEATHER_BAD_REQUEST_MESSAGE =
       "Looks like we took a wrong turn and couldn't find that city! Mind checking the map (spelling) again?";
@@ -27,15 +35,73 @@ public class ControllerAdviceUtil {
                 .session(UUID.randomUUID()));
   }
 
-  public static ResponseEntity<Error> getBadRequestError(String error, String message){
+  public static ResponseEntity<Error> getBadRequestError(
+      MissingServletRequestParameterException e) {
     return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body(
-                    new Error()
-                            .error(error)
-                            .message(message)
-                            .method("N/A")
-                            .timestamp(OffsetDateTime.now())
-                            .session(UUID.randomUUID()));
+        .body(
+            new Error()
+                .error(e.getClass().getSimpleName())
+                .message(e.getBody().getDetail())
+                .method("N/A")
+                .timestamp(OffsetDateTime.now())
+                .session(UUID.randomUUID()));
+  }
+
+  public static ResponseEntity<Error> getBadRequestError(ConstraintViolationException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(
+            new Error()
+                .error(e.getClass().getSimpleName())
+                .message(constraintViolationExceptionMessageFormatter(e))
+                .method("N/A")
+                .timestamp(OffsetDateTime.now())
+                .session(UUID.randomUUID()));
+  }
+
+  private static String constraintViolationExceptionMessageFormatter(
+      ConstraintViolationException e) {
+    String formattedMessage = "";
+
+    if (!e.getConstraintViolations().isEmpty()) {
+      ConstraintViolation<?> violation = e.getConstraintViolations().iterator().next();
+      String propertyPath = violation.getPropertyPath().toString();
+      String fieldName = propertyPath.substring(propertyPath.lastIndexOf('.') + 1);
+      String message = violation.getMessage();
+      formattedMessage = fieldName + ": " + message;
+    }
+
+    return formattedMessage;
+  }
+
+  public static ResponseEntity<Error> getBadRequestError(MethodArgumentTypeMismatchException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(
+            new Error()
+                .error(e.getClass().getSimpleName())
+                .message(e.getCause().getCause().getMessage())
+                .method("N/A")
+                .timestamp(OffsetDateTime.now())
+                .session(UUID.randomUUID()));
+  }
+
+  public static ResponseEntity<Error> getBadRequestError(MethodArgumentNotValidException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(
+            new Error()
+                .error(e.getClass().getSimpleName())
+                .message(methodArgumentNotValidExceptionMessageFormatter(e))
+                .method("N/A")
+                .timestamp(OffsetDateTime.now())
+                .session(UUID.randomUUID()));
+  }
+
+  @NotNull
+  private static String methodArgumentNotValidExceptionMessageFormatter(
+      MethodArgumentNotValidException e) {
+    return Objects.requireNonNull(e.getDetailMessageArguments())[1]
+        .toString()
+        .replace("[", "")
+        .replace("]", "");
   }
 
   public static ResponseEntity<Error> getWeatherBadRequestError(WeatherException e) {
