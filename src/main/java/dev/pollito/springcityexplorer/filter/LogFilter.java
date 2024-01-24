829b4fcb5330fea45800f76c8b90e5a7cfd5aab0
Fran Bec <franbecvort@gmail.com>
2024-01-24 10:46:52 +0000

    Logging request and response details

--- src/main/java/dev/pollito/springcityexplorer/filter/LogFilter.java
+++ src/main/java/dev/pollito/springcityexplorer/filter/LogFilter.java
@@ -7,10 +7,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 
+@Slf4j
 public class LogFilter implements Filter {
 
   @Override
@@ -19,9 +23,23 @@ public class LogFilter implements Filter {
       throws IOException, ServletException {
     MDC.put(MDC_SESSION_ID_KEY, UUID.randomUUID().toString());
     try {
+      logRequestDetails((HttpServletRequest) servletRequest);
       filterChain.doFilter(servletRequest, servletResponse);
+      logResponseDetails((HttpServletResponse) servletResponse);
     } finally {
       MDC.clear();
     }
+  }
+
+  private void logRequestDetails(HttpServletRequest request) {
+    log.info(
+        ">>>> Method: {}; URI: {}; QueryString: {}",
+        request.getMethod(),
+        request.getRequestURI(),
+        request.getQueryString());
+  }
+
+  private void logResponseDetails(HttpServletResponse response) {
+    log.info("<<<< Response Status: {}", response.getStatus());
   }
 }
